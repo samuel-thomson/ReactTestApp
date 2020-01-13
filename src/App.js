33878ca909492f0a29cc9de65e2bd737566658e4
Sam Thomson <samuelthomson62@gmail.com>
2020-01-13 10:40:26 -0500

    added a little clock for fun

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+const date = new Date()
+
 function App() {
   return (
-    <div className="App">ok</div>
+  <div className="App">It is currently {date.getHours() %12} o'clock!</div>
   );
 }
 
