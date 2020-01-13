320383688d58e79fd5e14e1c1a20b7730d8b2eb0
Sam Thomson <samuelthomson62@gmail.com>
2020-01-13 10:49:15 -0500

    added a timeOfDay funtion because I thought it was cool

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import './App.css';
 
 const date = new Date()
+const hours = date.getHours();
+let timeOfDay
+
+if(hours < 6) {
+  timeOfDay = "Why are you up right now?"
+}
+else if(hours >= 6 && hours <= 12) {
+  timeOfDay = "Good Morning!!!"
+}  
+else if(hours >= 12 && hours < 9) {
+  timeOfDay = "Good Afternoon!!!"
+}
+else if(hours <= 9) {
+  timeOfDay = "Good Night!"
+}
 
 function App() {
   return (
-  <div className="App">It is currently {date.getHours() %12} o'clock!</div>
+  <div className="App">It is currently {hours %12} o'clock!
+  <br />
+  {timeOfDay}
+  </div>
   );
 }
 
