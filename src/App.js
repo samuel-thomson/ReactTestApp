a7f2e33a9e76a7a8228d688d8b71bf57d6322076
Sam Thomson <samuelthomson62@gmail.com>
2020-01-14 00:00:54 -0500

    added some css to my component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import goodBoi from "./images/goodBoi.jpg"
 function App() {
   return (
   <div className="App">
+    <div className="PhotoCard">
       <PhotoCard 
         image={goodBoi}
         name="Good Boy"
         description="This is a pup named Tater down in Statesboro GA at a music festival called Hausfest <3"
       />
+    </div>
   </div>
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -3,7 +3,7 @@ import React from "react"
 function PhotoCard(props) {
     return (
         <div>
-            <img style={height 200} src={props.image} alt="well shoot!"/>
+            <img src={props.image} alt="well shoot!"/>
             <h3>{props.name}</h3>
             <p>{props.description}</p>
         </div>
