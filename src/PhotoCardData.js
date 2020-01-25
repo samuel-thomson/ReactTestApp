c6c97046623e911566379aa4c7e29175c3b99f36
Sam Thomson <samuelthomson62@gmail.com>
2020-01-25 16:30:06 -0500

    added some more data to my project. Still have yet to debug how to add in the images through React

--- src/PhotoCardData.js
+++ src/PhotoCardData.js
@@ -1,10 +1,25 @@
 import goodBoi from "./images/goodBoi.jpg"
+import Bronson from "./images/Bronson.jpg"
+import jerry from "./images/jerry.jpg"
+import Lindsey from "./images/Lindsey.jpg"
 
 const PhotoCardData = [
     {image: {goodBoi}, 
     name: "Good Boi",
     description: "This is a pup named Tater down in Statesboro GA at a music festival called Hausfest <3",
-  }
+    },
+    {image: {Bronson}, 
+    name: "Christian",
+    description: "Christian is my brother and best frined. We used to play in a band together. This is him in GA.",
+    },
+    {image: {jerry}, 
+    name: "Jerry",
+    description: "Jerry is a friend that used to play in a band called Space Cadets from NJ. We met him for the first time at Hausefest",
+    },
+    {image: {Lindsey}, 
+    name: "Lindsey",
+    description: "Lindsey is another good friend that I made in Georgia. They are an incredible artist. We got Waffle House together.",
+    }
 ]
 
 export default PhotoCardData
