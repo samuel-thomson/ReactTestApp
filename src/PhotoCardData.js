b2aff710aacf5b6e0c24889ab8471c07da9554bb
Sam Thomson <samuelthomson62@gmail.com>
2020-02-01 18:05:32 -0500

    Got my images to show through my data file. I'm a moron and was trying to escape JSX in a JS file -_-

--- src/PhotoCardData.js
+++ src/PhotoCardData.js
@@ -4,19 +4,19 @@ import jerry from "./images/jerry.jpg"
 import Lindsey from "./images/Lindsey.jpg"
 
 const PhotoCardData = [
-    {image: {goodBoi}, 
+    {image: goodBoi, 
     name: "Good Boi",
     description: "This is a pup named Tater down in Statesboro GA at a music festival called Hausfest <3",
     },
-    {image: {Bronson}, 
+    {image: Bronson, 
     name: "Christian",
     description: "Christian is my brother and best frined. We used to play in a band together. This is him in GA.",
     },
-    {image: {jerry}, 
+    {image: jerry, 
     name: "Jerry",
     description: "Jerry is a friend that used to play in a band called Space Cadets from NJ. We met him for the first time at Hausefest",
     },
-    {image: {Lindsey}, 
+    {image: Lindsey, 
     name: "Lindsey",
     description: "Lindsey is another good friend that I made in Georgia. They are an incredible artist. We got Waffle House together.",
     }
